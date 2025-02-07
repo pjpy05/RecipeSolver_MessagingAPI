4687453c776dea1a5b8ed5c348a0a78fb2569cba
INUI Takashi <pjpy05.2408@gmail.com>
2025-02-08 00:12:26 +0900

    巻き戻し

--- app.py
+++ app.py
@@ -73,4 +73,3 @@ debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=debug_mode)
-
